264dcdb7a03cf20997cd8b4461138eb53f1ff19e
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-12 11:47:38 -0500

    Add listBooks controller logic

--- back_end/src/controllers/listBooks.js
+++ back_end/src/controllers/listBooks.js
@@ -1,4 +1,15 @@
+import { getAllBooks } from '../services/getAllBooks.js';
+import { logInfo } from '../utils/logger.js';
+import { response } from '../utils/responseManager.js';
+
 // List all books
 export const listBooks = (req, res) => {
-  return;
+  try {
+    const books = getAllBooks();
+    logInfo(`Method: ${req.method} |`, `Path: ${req.path} |`, JSON.stringify(books) );
+
+    response(res, { type: 'success', name: 'listBooks', statusCode: 200 }, books );
+  } catch (err) {
+    response(res, { type: 'error', name: 'listBooks', statusCode: 500 }, { message: err.message } )
+  }
 };
