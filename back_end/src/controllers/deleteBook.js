011479e378119f99104c4ef3e4cb1b7bf14a1230
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-12 11:47:18 -0500

    Add deleteBook controller logic

--- back_end/src/controllers/deleteBook.js
+++ back_end/src/controllers/deleteBook.js
@@ -1,4 +1,24 @@
+import { validateProperties } from '../validations/validations.js';
+import { idSchema } from '../validations/schemas.js';
+import { deleteBookEntry } from '../services/deleteBookEntry.js';
+import { logInfo } from '../utils/logger.js';
+import { response } from '../utils/responseManager.js';
+
 // Delete a book entry
 export const deleteBook = (req, res) => {
-  return;
+  const { id } = req.params;
+
+  // Validate id properties
+  const validationResult = validateProperties(idSchema, id);
+  if (validationResult.errorCode !== 0)
+    return response(res, { type: 'error', name: 'deleteBook', statusCode: validationResult.statusCode }, { message: validationResult.message } );
+  
+  try {
+    const result = deleteBookEntry(id);
+    logInfo( `Method: ${req.method} |`, `Path: ${req.path} |`, JSON.stringify(result) );
+
+    response( res, { type: 'success', name: 'deleteBook', statusCode: 200 }, { message: result.message } );
+  } catch (err) {
+    response( res, { type: 'error', name: 'deleteBook', statusCode: 500 }, { message: err.message } );
+  }
 };
