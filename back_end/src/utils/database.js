160b60dc32653bdfa272402494bbf135165e70e2
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-12 11:50:49 -0500

    Add book structure example

--- back_end/src/utils/database.js
+++ back_end/src/utils/database.js
@@ -1,1 +1,14 @@
-export const booksStorage = [];
+import { randomUUID } from 'node:crypto';
+
+export const booksStorage = [
+  // Book example
+  {
+    id: randomUUID(),
+    title: 'Percy Jackson & the Olympians: The Lightning Thief',
+    author: 'Rick Riordan',
+    year: 2005,
+    price: 25,
+    stock: 10,
+    picture: 'https://postimg.cc/nX8pPqMy'
+  }
+];
