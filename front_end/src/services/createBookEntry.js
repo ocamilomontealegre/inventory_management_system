53bfeea4157ff42c6e9f5ac268544970c16f5a47
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 10:24:31 -0500

    Update createBookEntry service

--- front_end/src/services/createBookEntry.js
+++ front_end/src/services/createBookEntry.js
@@ -4,7 +4,6 @@ import { baseUrl } from '../api/apiData.js';
 export const createBookEntry = async (bookData) => {
   try {
     const result = await axios.post(baseUrl, bookData);
-    console.log('🐲 ~ file: createBookEntry.js:7 ~ createBookEntry ~ result:', result);
     return result;
   } catch (err) {
     console.error(err);
