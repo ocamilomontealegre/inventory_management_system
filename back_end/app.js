50d1e8535f42849346ea745bd9cc5aee3483c480
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-12 20:59:22 -0500

    Add cors module

--- back_end/app.js
+++ back_end/app.js
@@ -1,7 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors';
 import routes from './src/routes/routes.js';
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Parse body request
 app.use(json());
