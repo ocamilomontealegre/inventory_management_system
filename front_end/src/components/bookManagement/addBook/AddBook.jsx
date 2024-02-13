39e55392ba3ee127103632e7155c4313ca46b099
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 10:22:03 -0500

    Update AddBook component

--- front_end/src/components/bookManagement/addBook/AddBook.jsx
+++ front_end/src/components/bookManagement/addBook/AddBook.jsx
@@ -2,8 +2,6 @@ import { ServiceTitle } from "../../common/ServiceTitle.jsx";
 import { BookForm } from "./BookForm.jsx";
 
 export const AddBook = () => {
-
-
   return(
     <div className="f-message"> 
       <ServiceTitle text="Create New Book Entry" />
