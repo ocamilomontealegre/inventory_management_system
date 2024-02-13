7b1ae24f034e7760ca5638cd30ce093ab24bc30b
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 10:07:46 -0500

    Modify updateBookEntry service

--- back_end/src/services/updateBookEntry.js
+++ back_end/src/services/updateBookEntry.js
@@ -3,9 +3,15 @@ import { findBookIndex } from '../utils/functions.js';
 
 // Update book entry properties
 export const updateBookEntry = (bookId, updatedBookData) => {
+  const nonEmptyProperties = Object.entries(updatedBookData)
+    .reduce((acc, [key, value]) => {
+      if(value !== '') acc[key] = value;
+      return acc;
+    }, {});
+
   try {
     const bookIndex = findBookIndex(booksStorage, bookId);
-    Object.assign(booksStorage[bookIndex], updatedBookData);
+    Object.assign(booksStorage[bookIndex], nonEmptyProperties);
     return { message: booksStorage[bookIndex] };
   } catch (err) {
     return { message: err.message };
