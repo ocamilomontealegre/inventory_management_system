12325384654d229d71ceefb6dc871ce6e6cc43ac
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 10:23:01 -0500

    Update DeleteBook component

--- front_end/src/components/bookManagement/deleteBook/DeleteBook.jsx
+++ front_end/src/components/bookManagement/deleteBook/DeleteBook.jsx
@@ -1,7 +1,11 @@
+import { ServiceTitle } from "../../common/ServiceTitle.jsx";
+import { DeleteForm } from "./DeleteForm.jsx";
+
 export const DeleteBook = () => {
   return(
-    <div>
-
+    <div className="f-message">
+      <ServiceTitle text="Delete Book Entry" />
+      <DeleteForm />
     </div>
-  )
+  );
 };
